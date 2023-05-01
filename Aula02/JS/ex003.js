7588a8f9176ec057d70ab594878d4bc0eedb1911
Matheus Gabriel <matheus.gssilva19@gmail.com>
2023-05-01 16:36:31 -0300

    Summary

---
+++ Aula02/JS/ex003.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt("Digite um número:")) //String
+var n2 = Number.parseInt(window.prompt("Digite outro número")) //String
+var soma = n1 + n2
+window.alert (`A soma entre ${n1} e ${n2}, é igual ${soma}`) //templete string
---
+++ Aula02/JS/ex004.js
@@ -0,0 +1,4 @@
+var nome = window.prompt("Qual o seu nome?")
+document.write(`Olá ${nome}, s.eu nome tem ${nome.length} letras <br>`)
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br>`)
+document.write(`Seu nome em maiúscula é ${nome.toLowerCase()}`)
